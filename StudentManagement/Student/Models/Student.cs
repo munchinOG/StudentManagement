e5ad60c0e3e228b5f45d9f4ec233eeebc7e1161c
Michael Etim <michaelmunchin89@gmail.com>
2020-08-11 15:00:42 +0100

    ModelValidation Added Validation Attributes in Properties In the HomeController, check if validation has failed or succeeded Added TagHelpers Validation, asp-validation-for and asp-validation-summary

--- StudentManagement/Student/Models/Student.cs
+++ StudentManagement/Student/Models/Student.cs
@@ -1,9 +1,17 @@
-﻿namespace Student.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Student.Models
 {
     public class Student
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength( 50, ErrorMessage = "Name cannot exceed 50 characters" )]
         public string Name { get; set; }
+        [Required]
+        [RegularExpression( @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+            ErrorMessage = "Invalid Email Format" )]
+        [Display( Name = "Office Email" )]
         public string Email { get; set; }
         public Dept Department { get; set; }
     }
