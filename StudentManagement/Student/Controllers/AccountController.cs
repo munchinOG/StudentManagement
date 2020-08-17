46d03d5a9b9af9a454bb67548b2b8f632c51bd71
Michael Etim <michaelmunchin89@gmail.com>
2020-08-17 22:24:19 +0100

    Implementing Login and Logout

--- StudentManagement/Student/Controllers/AccountController.cs
+++ StudentManagement/Student/Controllers/AccountController.cs
@@ -16,6 +16,13 @@ namespace Student.Controllers
             _signInManager = signInManager;
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Logout( )
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction( "Index", "Home" );
+        }
+
         [HttpGet]
         public IActionResult Register( )
         {
@@ -27,7 +34,11 @@ namespace Student.Controllers
         {
             if(ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+                var user = new IdentityUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email
+                };
                 var result = await _userManager.CreateAsync( user, model.Password );
 
                 if(result.Succeeded)
@@ -40,6 +51,30 @@ namespace Student.Controllers
                 {
                     ModelState.AddModelError( "", error.Description );
                 }
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login( )
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login( LoginViewModel model )
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync( model.Email, model.Password,
+                    model.RememberMe, false );
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction( "Index", "Home" );
+                }
+                ModelState.AddModelError( string.Empty, "Invalid Login Attempt" );
             }
 
             return View();
