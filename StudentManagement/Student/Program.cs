286b94718494ffacf229c46fcdf4ed6849fb9574
Michael Etim <michaelmunchin89@gmail.com>
2020-08-17 14:55:55 +0100

    Added nlog.config

--- StudentManagement/Student/Program.cs
+++ StudentManagement/Student/Program.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 
 namespace Student
 {
@@ -18,6 +15,14 @@ namespace Student
 
         public static IHostBuilder CreateHostBuilder( string[] args ) =>
             Host.CreateDefaultBuilder( args )
+                .ConfigureLogging( ( hostingContext, logging ) =>
+                 {
+                     logging.AddConfiguration( hostingContext.Configuration.GetSection( "Logging" ) );
+                     logging.AddConsole();
+                     logging.AddDebug();
+                     logging.AddEventSourceLogger();
+                     logging.AddNLog();
+                 } )
                 .ConfigureWebHostDefaults( webBuilder =>
                  {
                      webBuilder.UseStartup<Startup>();
