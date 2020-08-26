d333328996ee12c4a932c792d0a6e44bc650c846
Michael Etim <michaelmunchin89@gmail.com>
2020-08-26 17:44:48 +0100

    Added ExternalLoginCallback action

--- StudentManagement/Student/Controllers/AccountController.cs
+++ StudentManagement/Student/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Student.Models;
 using Student.ViewModels;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Student.Controllers
@@ -130,6 +131,72 @@ namespace Student.Controllers
 
             var properties = _signInManager.ConfigureExternalAuthenticationProperties( provider, redirectUrl );
             return new ChallengeResult( provider, properties );
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult>
+            ExternalLoginCallBack( string returnUrl = null, string remoteError = null )
+        {
+            returnUrl = returnUrl ?? Url.Content( "~/" );
+
+            LoginViewModel loginViewModel = new LoginViewModel
+            {
+                ReturnUrl = returnUrl,
+                ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList()
+            };
+
+            if(remoteError != null)
+            {
+                ModelState.AddModelError( string.Empty, $"Error from external provider: {remoteError}" );
+
+                return View( "Login", loginViewModel );
+            }
+
+            var info = await _signInManager.GetExternalLoginInfoAsync();
+            if(info == null)
+            {
+                ModelState.AddModelError( string.Empty, "Error loading external login information." );
+
+                return View( "Login", loginViewModel );
+            }
+
+            var signInResult = await _signInManager.ExternalLoginSignInAsync( info.LoginProvider, info.ProviderKey,
+                isPersistent: false, bypassTwoFactor: true );
+
+            if(signInResult.Succeeded)
+            {
+                return LocalRedirect( returnUrl );
+            }
+            else
+            {
+                var email = info.Principal.FindFirstValue( ClaimTypes.Email );
+
+                if(email != null)
+                {
+                    var user = await _userManager.FindByEmailAsync( email );
+
+                    if(user == null)
+                    {
+                        user = new ApplicationUser
+                        {
+                            UserName = info.Principal.FindFirstValue( ClaimTypes.Email ),
+                            Email = info.Principal.FindFirstValue( ClaimTypes.Email )
+                        };
+
+                        await _userManager.CreateAsync( user );
+                    }
+
+                    await _userManager.AddLoginAsync( user, info );
+                    await _signInManager.SignInAsync( user, isPersistent: false );
+
+                    return LocalRedirect( returnUrl );
+                }
+
+                ViewBag.ErrorTitle = $"Email claim not received from: {info.LoginProvider}";
+                ViewBag.ErrorMessage = "Please contact support on Student@Munchintech.com";
+
+                return View( "Error" );
+            }
         }
     }
 }
